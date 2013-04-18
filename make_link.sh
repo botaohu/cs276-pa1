3900c9dc3937cfce39d2ea796667b338db825a01
Botao Hu <me@botao.hu>
2013-04-18 13:35:51 -0700

    asd

--- make_link.sh
+++ make_link.sh
@@ -1,18 +1,18 @@
 cd task1
-ln -s ../code/query.py 
-ln -s ../code/corpus.py 
-ln -s ../code/index.py
+cp -f ../code/query.py query.py
+cp -f ../code/corpus.py corpus.py 
+cp -f ../code/index.py index.py
 
 cd ..
 
 cd task2
-ln -s ../code/query.py 
-ln -s ../code/corpus.py 
-ln -s ../code/index.py
+cp -f ../code/query.py query.py
+cp -f ../code/corpus.py  corpus.py  
+cp -f ../code/index.py index.py
 cd ..
 
 cd extra_credit
-ln -s ../code/query.py 
-ln -s ../code/corpus.py 
-ln -s ../code/index.py
+cp -f ../code/query.py query.py
+cp -f ../code/corpus.py  corpus.py 
+cp -f ../code/index.py index.py
 cd ..
